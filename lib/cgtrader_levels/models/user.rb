3e63c1b98e0058ea51231cc615bb97d608c4a76f
mihkelal <mihkel.alavere@eesti.ee>
2021-02-14 11:03:48 +0200

    Refactor CgtraderLevels::User

--- lib/cgtrader_levels/models/user.rb
+++ lib/cgtrader_levels/models/user.rb
@@ -1,39 +1,35 @@
 class CgtraderLevels::User < ActiveRecord::Base
-  attr_reader :level
+  belongs_to :level
 
   after_initialize do
-    self.reputation = 0
+    self.reputation ||= 0
     set_corresponding_level
   end
 
-  before_save do
-    set_corresponding_level_and_grant_level_up_bonuses
-  end
+  before_save :set_corresponding_level_and_grant_level_up_bonuses
 
   private
 
   def set_corresponding_level
-    corresponding_level = find_corresponding_level
-
-    if corresponding_level
-      self.level_id = corresponding_level.id
-      @level = corresponding_level
-    end
+    self.level ||= find_corresponding_level
   end
 
   def set_corresponding_level_and_grant_level_up_bonuses
     new_corresponding_level = find_corresponding_level
     return if new_corresponding_level == self.level
 
-    level_ups = level_ups_count(new_corresponding_level)
-    self.coins += 7 * level_ups
-    self.tax -= 1 * level_ups
-    self.level_id = new_corresponding_level.id
-    @level = new_corresponding_level
+    grant_level_up_bonuses(new_corresponding_level)
+    self.level = new_corresponding_level
   end
 
   def find_corresponding_level
-    CgtraderLevels::Level.where(experience: ..reputation).order(experience: :desc).first
+    CgtraderLevels::Level.where(experience: ..reputation).order(:experience).last
+  end
+
+  def grant_level_up_bonuses(corresponding_level)
+    level_ups = level_ups_count(corresponding_level)
+    self.coins += 7 * level_ups
+    self.tax -= 1 * level_ups
   end
 
   def level_ups_count(corresponding_level)
